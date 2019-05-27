e3f2286e6224bdc8ae84f0a3583b1e418a17a33d
SolarieofAstora <44851741+SolarieofAstora@users.noreply.github.com>
2019-05-27 00:51:51 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ Integrantes:
 180032518 - Guilherme Chagas Suzuki
 
 160144884 - Saulo Mendes Feitosa
+
+O projeto utiliza uma biblioteca gráfica chamada Ebiten para gerar o grid, para roda-la é necessário ter instalado o go 1.12.
+Segue abaixo o link com tutorial para a instalação da biblioteca:
+https://ebiten.org/install.html
--- main.go
+++ main.go
@@ -7,9 +7,20 @@ import (
     	"os"
     	"os/exec"
     	"runtime"
+
+        _ "image/png"
+        "log"
+        "image/color"
+
+        "github.com/hajimehoshi/ebiten"
+        "github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
 var clear map[string]func() //create a map for storing clear funcs
+var img *ebiten.Image //Imagem 1 quadrado preto
+var img2 *ebiten.Image //Imagem 2 quadrado branco
+var tela [][]Celula // canal para grid de print
+var teste int = 0 // variavel para teste de controle do grid
 
 func init() {
     clear = make(map[string]func()) //Initialize it
@@ -23,6 +34,14 @@ func init() {
         cmd.Stdout = os.Stdout
         cmd.Run()
     }
+    //Parte Grafica
+    var err error
+	img, _, err = ebitenutil.NewImageFromFile("square.png", ebiten.FilterDefault)
+	img2, _, err = ebitenutil.NewImageFromFile("blank.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
 
 func CallClear() {
@@ -32,6 +51,53 @@ func CallClear() {
     } else { //unsupported platform
         panic("Your platform is unsupported! I can't clear terminal screen :(")
     }
+}
+
+func update(screen *ebiten.Image) error{
+	if ebiten.IsDrawingSkipped() {
+		return nil
+	}
+	//ebitenutil.DebugPrint(screen, "Hello, World!")
+	screen.Fill(color.RGBA{0, 0xff, 0, 0xff})
+	
+	op := &ebiten.DrawImageOptions{}
+	//op.GeoM.Translate(1, 1)
+	//screen.DrawImage(img, op))
+	
+	var grid [][]Celula
+	grid = tela
+
+	//x := 0.0
+	for j := 0; j < 38; j++ {
+		op.GeoM.Translate(grid[0][j].posX, grid[0][j].posY)
+        if(grid[0][j].viva == true){
+		          screen.DrawImage(img2, op)
+        } else {
+            screen.DrawImage(img, op)
+        }
+        op.GeoM.Reset()
+		//x-=11.0
+	}
+	//op.GeoM.Translate(x, 0)
+	//x = 0.0
+
+	for i := 0; i < 28; i++ {
+		//op.GeoM.Translate(0, 11)
+		//screen.DrawImage(img, op)
+		for j := 0; j < 38; j++ {
+    		op.GeoM.Translate(grid[i][j].posX, grid[i][j].posY)
+            if(grid[i][j].viva == true){
+    		          screen.DrawImage(img2, op)
+            } else {
+                screen.DrawImage(img, op)
+            }
+            op.GeoM.Reset()
+			//x-=11.0
+		}
+		//op.GeoM.Translate(x, 0)
+		//x = 0.0
+	}
+	return nil
 }
 
 //Conjunto de Genes para sobreviver a regiões que cada celula pode ter
@@ -48,6 +114,8 @@ type Celula struct{
     viva bool
     especie int
     mutacoes Genes
+    posX float64
+    posY float64
 }
 
 
@@ -107,8 +175,8 @@ func MostraGrid(N int, M int, grid [][]Celula){
     for i := 0; i < N; i++ {
         for j := 0; j < M; j++ {
 		if(grid[i][j].viva){
-			fmt.Printf("#") 
-		} else { 
+			fmt.Printf("#")
+		} else {
 			fmt.Printf(" ")
 		}
         }
@@ -124,7 +192,7 @@ type Old_State struct{
 
 type Rule [512]bool
 
-func Jogo(rule Rule,N int, M int,grid [][]Celula, i int, j int, hold chan int, result chan Old_State) {
+func Jogo(rule Rule,N int, M int, i int, j int, hold chan int, result chan Old_State,grid [][]Celula) {
     	//Verifica todos os vizinhos da celula
     	domain := bitfy(i,j,N,M,grid)
 
@@ -133,71 +201,104 @@ func Jogo(rule Rule,N int, M int,grid [][]Celula, i int, j int, hold chan int, r
     	ret.i= i
     	ret.j= j
     	ret.vida = apply_rule(rule,domain)
-    	
+
     	hold <- 1
     	result <- ret
 }
 
-func AtualizaGrid(N int,M int,grid [][]Celula){
-    	cont := 0
-    	continua := true
-	//conway := rule_conway()
-	conway := rule_random()
 
-    	for continua != false {
-		
-        	CallClear()
-        	MostraGrid(N,M,grid)
-        	time.Sleep(100 * time.Millisecond)
 
-		hold := make(chan int,0)
-		result := make(chan Old_State)
-		
-        	for i := 0; i < N; i++ {
-        		for j := 0; j < M; j++ {
-        	        	go Jogo(conway,N,M,grid,i,j,hold,result)
-        	    	}
-        	}
+func AtualizaGrid(rule Rule,grid [][]Celula,N int,M int){
+	hold := make(chan int,0)
+	result := make(chan Old_State)
 
-		for i := 0; i < N*M; i++{
-			<-hold
-		}
-
-		var new Old_State
-		for i := 0; i < N*M; i++{
-			new = <-result
-			grid[new.i][new.j].viva = new.vida
-		}
-
-        	//fmt.Printf("\n")
-
-        	if(cont == 100){//Garantir que não rode infinitamente
-        		continua = false
-        	}
+    	for i := 0; i < N; i++ {
+    		for j := 0; j < M; j++ {
+     	   		go Jogo(rule,N,M,i,j,hold,result,grid)
+    		}
     	}
+
+	for i := 0; i < N*M; i++{
+		<-hold
+	}
+
+	var new Old_State
+	for i := 0; i < N*M; i++{
+		new = <-result
+		grid[new.i][new.j].viva = new.vida
+	}
 }
 
-func Teste (N int,M int,Celulas [][]Celula) {
+func Teste (grid [][]Celula,N int,M int) {
+    eixoX, eixoY := 1.0, 1.0
+
 	for i:=0;i<N;i++ {
 		for j:=0;j<M;j++{
-			Celulas[i][j].viva=false
+			grid[i][j].viva=false
+            grid[i][j].posX=eixoX
+            grid[i][j].posY=eixoY
+            eixoX+=11.0
 		}
+        	eixoX=1.0
+        	eixoY+=11.0
 	}
-	Celulas[0][1].viva = true
-	Celulas[1][2].viva = true
-	Celulas[2][0].viva = true
-	Celulas[2][1].viva = true
-	Celulas[2][2].viva = true
+	grid[0][1].viva = true
+	grid[1][2].viva = true
+	grid[2][0].viva = true
+	grid[2][1].viva = true
+	grid[2][2].viva = true
+
+}
+
+func simulate(grid [][]Celula){		
+	conway := rule_conway()
+	//conway := rule_random()
+
+	teste := 0
+	for true {
+    		if(teste >= 20){
+    	    		AtualizaGrid(conway,grid,100,100)
+    	    		// Modificações para testar que o grid se altera
+    	    		teste = 0
+    		}
+		teste++
+	}
+}
+
+func chose(grid1 [][]Celula,grid2 [][]Celula){
+	for cont:=0; true; cont = (cont+1)%2000 {
+		if(cont < 1000){
+			tela = grid1
+		} else {
+			tela = grid2
+		}	
+	}
 }
 
 func main(){
-    	grid := make([][]Celula,100,100) 
-	for i := range grid {
-    		grid[i] = make([]Celula,100,100)
+	simulacao1 := make([][]Celula,100,100)
+	for i := range simulacao1 {
+    		simulacao1[i] = make([]Celula,100,100)
 	}
 
-    	Teste(100,100,grid)
-    	//IniciaGrid(100,100,grid)
-    	MostraGrid(100,100,grid)
-    	AtualizaGrid(100,100,grid)
+	simulacao2 := make([][]Celula,100,100)
+	for i := range simulacao2 {
+    		simulacao2[i] = make([]Celula,100,100)
+	}
+
+   	Teste(simulacao1,100,100)
+	IniciaGrid(100,100,simulacao2)
+
+
+	go simulate(simulacao1)
+	go simulate(simulacao2)
+
+	go chose(simulacao1,simulacao2)
+
+    	if err := ebiten.Run(update, 420, 310, 2, "Novo jogo da vida"); err != nil {
+		log.Fatal(err)
+	}
+    	
+    	//MostraGrid(100,100,grid)
+
 }
